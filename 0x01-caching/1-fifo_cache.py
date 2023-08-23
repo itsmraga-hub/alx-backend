ecb28a0f4f3fb59aad97d38ae3ebb8f56ff30375
itsmraga-hub <ragawilliam570@gmail.com>
2023-08-23 16:05:55 +0300

    Task 1

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+"""
+    class FIFOCache that inherits from BaseCaching and is a caching system:
+"""
+from collections import OrderedDict
+
+from base_caching import BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """
+        class FIFOCache that inherits from BaseCaching and is a caching
+        system:
+    """
+    def __init__(self):
+        """
+            Initializes the cache.
+        """
+        super().__init__()
+        self.cache_data = OrderedDict()
+
+    def put(self, key, item):
+        """
+            Adds an item in the cache.
+        """
+        if key is None or item is None:
+            return
+        self.cache_data[key] = item
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            first_key, _ = self.cache_data.popitem(False)
+            print("DISCARD:", first_key)
+
+    def get(self, key):
+        """
+            Retrieves an item by key.
+        """
+        return self.cache_data.get(key, None)
